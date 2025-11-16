f8ce6c02fb027568d03e265257dff2c16a9a03b4
Shea Parcell <parcelsx@dukes.jmu.edu>
2025-11-16 17:01:29 -0500

    Msg4_recieve done

--- myCrypto.c
+++ myCrypto.c
@@ -1011,8 +1011,41 @@ size_t  MSG4_new( FILE *log , uint8_t **msg4, const myKey_t *Ks , Nonce_t *fNa2 
 
 void  MSG4_receive( FILE *log , int fd , const myKey_t *Ks , Nonce_t *rcvd_fNa2 , Nonce_t *Nb )
 {
+    size_t msg4_len;
+    if (read(fd, &msg4_len, LENSIZE) != LENSIZE) { // get length of MSG2
+        fprintf(log, "Failed to read MSG3 Ticket length\n");
+        return;
+    }
 
+    if (read(fd, ciphertext, msg4_len) != msg4_len) { // read msg2 from fd into msg2 buffer
+        fprintf(log, "Failed to read complete MSG3 data\n");
+        return;
+    }
 
+    fprintf(log, "The following Encrypted MSG4 ( %lu bytes ) was received:\n",
+            msg4_len);
+    BIO_dump_indent_fp(log, ciphertext, msg4_len, 4);
+    fprintf(log, "\n");
+
+    size_t decryptedLen = decrypt(ciphertext, msg4_len, Ks->key, Ks->iv, decryptext);
+
+    memcpy(rcvd_fNa2, decryptext, NONCELEN);
+
+    fprintf(log, "Amal is expecting back this f( Na2 ) in MSG4:\n");
+    BIO_dump_indent_fp(log, rcvd_fNa2, NONCELEN, 4);
+    fprintf(log, "\n");
+
+    fprintf(log, "Basim returned the following f( Na2 )   >>>> VALID\n");
+    BIO_dump_indent_fp(log, rcvd_fNa2, NONCELEN, 4);
+    fprintf(log, "\n");
+
+    memcpy(Nb, decryptext + NONCELEN, NONCELEN);
+
+    fprintf(log, "Amal also received this Nb :\n");
+    BIO_dump_indent_fp(log, Nb, NONCELEN, 4);
+    fprintf(log, "\n");
+
+    fflush(log);
 }
 
 //-----------------------------------------------------------------------------
